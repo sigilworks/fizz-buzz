7df7c63bf4a68d1dbb8841ab7fc9354d64ee1fd6
sigilworks <tim@sigilworks.com>
2013-01-08 00:01:38 -0500

    113 chars...probably the best I can get it.

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -4,5 +4,5 @@
 // 115 characters
 !function(){var i=1,r,a='fizz',b='buzz';while(i<100)r=i,console.log([r,!(i%15)?a+b:!(i%5)?b:!(i%3)?a:'']+''),++i}()
 
-// 114 characters
-!function(a,b,i,r){while(i<100)r=d=e=i,d%=5,e%=3,console.log([r,!d&&!e?a+b:!d?b:!e?a:'']+''),++i}('fizz','buzz',1)
+// 113 characters
+!function(a,b,i,r){while(i<100)r=d=e=i,d%=5,e%=3,console.log([r,!d&!e?a+b:!d?b:!e?a:'']+''),++i}('fizz','buzz',1)
