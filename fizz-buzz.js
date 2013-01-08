694291bbb807fd65734e1f1f27cd8602b7349b7d
sigilworks <tim@sigilworks.com>
2013-01-07 23:27:09 -0500

    Added 115 char version.

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,1 +1,5 @@
+// 120 characters
 !function(){var i=1,r;while(i<100)r=i,console.log(r+=!(i%15)?': fizzbuzz!':!(i%5)?': buzz!':!(i%3)?': fizz!':''),i++;}()
+
+// 115 characters
+!function(){var i=1,r,a='fizz',b='buzz';while(i<100)r=i,console.log([r,!(i%15)?a+b:!(i%5)?b:!(i%3)?a:'']+''),++i}()
