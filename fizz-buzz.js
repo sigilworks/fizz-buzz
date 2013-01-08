c00091ceace9e24676d1fbb4c21328a4d0515a53
sigilworks <tim@sigilworks.com>
2013-01-07 23:48:41 -0500

    114 chars!

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -3,3 +3,6 @@
 
 // 115 characters
 !function(){var i=1,r,a='fizz',b='buzz';while(i<100)r=i,console.log([r,!(i%15)?a+b:!(i%5)?b:!(i%3)?a:'']+''),++i}()
+
+// 114 characters
+!function(a,b,i,r){while(i<100)r=d=e=i,d%=5,e%=3,console.log([r,!d&&!e?a+b:!d?b:!e?a:'']+''),++i}('fizz','buzz',1)
